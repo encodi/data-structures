c6f69b57e94e60b9023d92290fb3a2f100d561d1
Enrique C <encodari@gmail.com>
2022-08-17 12:17:10 -0500

    feat: adds stack and queue implementation using linked lists

---
+++ src/ds/stacksAndQueues.js
@@ -0,0 +1,83 @@
+/**
+ * Stack with LL type of implementation
+ * LIFO
+ * Big O
+ * Insertion O(1)
+ * Remove O(1)
+ * Searching O(n)
+ * Access O(n)
+ */
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      let temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return ++this.size;
+  }
+  pop() {
+    if (!this.first) return null;
+    let temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
+
+/**
+ * Queue with LL type of implementation
+ * FIFO
+ * Big O
+ * Insertion O(1)
+ * Remove O(1)
+ * Searching O(n)
+ * Access O(n)
+ */
+class Queue {
+  constructor() { // NOSONAR
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  enqueue(val) {
+    let newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return ++this.size;
+  }
+  dequeue() { // NOSONAR
+    if (!this.first) return null;
+    let temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
